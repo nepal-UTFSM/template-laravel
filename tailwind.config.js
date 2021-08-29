55a8ef573a0f969e176844b91965f5b59e4e8430
vicholpe <vlinerospardo@gmail.com>
2021-08-29 05:25:32 -0400

    fix: linter

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,4 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
   ],
-}
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue();
 
-mix.postCss("resources/css/app.css", "public/css", [
-  require("tailwindcss"),
+mix.postCss('resources/css/app.css', 'public/css', [
+  require('tailwindcss'),
 ]);
