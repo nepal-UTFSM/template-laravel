9fe3a31f54804bc19fe3c5c99502089797123fc8
vicholpe <vlinerospardo@gmail.com>
2021-09-05 08:15:26 -0300

    feat(mix): update webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue();
 
-mix.postCss('resources/css/app.css', 'public/css', [
-  require('tailwindcss'),
-]);
+mix.postCss('resources/css/app.css', 'public/css');
+
+if (mix.inProduction()) {
+  mix.version();
+  mix.extract();
+}
