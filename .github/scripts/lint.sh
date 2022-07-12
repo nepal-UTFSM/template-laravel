2685a3ed8b39c4ac5b91dbcbe473a8e118996e17
vicholpe <vlinerospardo@gmail.com>
2022-07-12 04:25:16 -0400

    wip

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-find . -path ./vendor -prune -o -path ./storage -prune -o -type f -name '*.php' -exec php -l {} 2>&1 >/dev/null \; | sed -r "s|PHP ([[:graph:][:space:]]*) in ([[:graph:]]*) on line ([[:digit:]]*)|::error file=\2,line=\3,col=0::\1|m"
-./vendor/bin/phpstan --memory-limit=1G --no-progress --error-format=github
-./vendor/bin/phpcs app/ --report=emacs | sed -r "s|([[:graph:]]*):([[:digit:]]*):([[:digit:]]*): ([[:graph:][:space:]]*)|::error file=\1,line=\2,col=\3::\4|m"
+result=0
+trap 'result=1' ERR
+find . -path ./vendor -prune -o -path ./storage -prune -o -type f -name '*.php' -exec php -l {} 2>&1 >/dev/null \; > phplint.txt
+cat phplint.txt | sed -r "s|PHP ([[:graph:][:space:]]*) in ([[:graph:]]*) on line ([[:digit:]]*)|::error file=\2,line=\3,col=0::\1|m"
 
-./node_modules/.bin/eslint "./resources/**/*.{js,vue}" --format unix | sed -r "s|([[:graph:]]*):([[:digit:]]*):([[:digit:]]*): ([[:graph:][:space:]]*)|::error file=\1,line=\2,col=\3::\4|m"
+./vendor/bin/phpstan --memory-limit=1G --no-progress --error-format=github
+
+./vendor/bin/phpcs app/ --report=emacs > phpcs.txt
+cat phpcs.txt | sed -r "s|([[:graph:]]*):([[:digit:]]*):([[:digit:]]*): ([[:graph:][:space:]]*)|::error file=\1,line=\2,col=\3::\4|m"
+
+./node_modules/.bin/eslint "./resources/**/*.{js,vue}" --format unix > eslint.txt
+cat eslint.txt | sed -r "s|([[:graph:]]*):([[:digit:]]*):([[:digit:]]*): ([[:graph:][:space:]]*)|::error file=\1,line=\2,col=\3::\4|m"
+
+rm phpcs.txt eslint.txt phplint.txt
+exit "$result"
