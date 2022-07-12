88ea14c62dd816bf790ec629dc3eb67c4f17c88d
vicholpe <vlinerospardo@gmail.com>
2022-07-12 01:31:22 -0400

    wip

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request): RedirectResponse a
+    public function login(LoginRequest $request): RedirectResponse
     {
         if (Auth::attempt($request->validated())) {
             $request->session()->regenerate();
