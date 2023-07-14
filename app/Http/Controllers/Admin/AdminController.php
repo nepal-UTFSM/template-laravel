46829604a68ef9e36aa994b936c954b781a4f0c9
vicholp <vlinerospardo@gmail.com>
2023-07-14 12:54:31 -0400

    update deps

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -9,6 +9,6 @@ class AdminController extends Controller
 {
     public function index(): View
     {
-        return view('admin.index'); // @phpstan-ignore-line
+        return view('admin.index');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Services\RoleService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
